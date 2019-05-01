1610e948419b7d0e6f279ee9a4fcfc25af0634ec
Nepcam <cameron@aux.network>
2019-05-01 15:05:13 +1200

    test

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-console.log('Hello World')
-console.error('This is an error')
+// console.log('Hello World')
+// console.error('This is an error')
+// console.warn('This is an warning')
+
+// variables let, const. 
+// let' you can reassign variables
+// const' constant, it can't changed or reassigned.
+// Always use const - unless you are going to reassign the value
+
+let age = 30;
+age = 31;
+
+console.log(age);
+
