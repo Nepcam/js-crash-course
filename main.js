e22bf4ee911716f1f19566ea4f82b61fc2ec531d
Nepcam <cameron@aux.network>
2019-05-02 12:23:57 +1200

    commit testing

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// console.log('Hello World')
-// console.error('This is an error')
-// console.warn('This is an warning')
+console.log('Hello World')
+console.error('This is an error')
+console.warn('This is an warning')
 
 
 // --Variables--
@@ -9,37 +9,37 @@
 // const' constant, it can't changed or reassigned.
 // Always use const - unless you know you are going to reassign the value
 
-// let age = 30;
-// age = 31;
+let age = 30;
+age = 31;
 
-// console.log(age);
+console.log(age);
 
-// let score;
-// score = 10;
+let score;
+score = 10;
 
-// console.log(score)
+console.log(score)
 
 //--Data Type--
 // String, Numbers, Booleon, null, undefined
 
-// const name = 'Cam';
-// const age = 30;
-// const rating = 4.5;
-// const isCool = true;
-// const x = null;
-// const y = undefined;
-// let z;
+const name = 'Cam';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
 
-// console.log(typeof x)
+console.log(typeof x)
 
 // Concatenation
-//console.log('My name is ' + name + ' and I am ' + age + ' years old, and I am ' )
+console.log('My name is ' + name + ' and I am ' + age + ' years old, and I am ' )
 // Template String
-//console.log(`My name is ${name} and I am ${age}`)
+console.log(`My name is ${name} and I am ${age}`)
 
-// const s = 'technology, computers, it, code';
+const s = 'technology, computers, it, code';
 
-// console.log(s.split(', '));
+console.log(s.split(', '));
 
 /* I am
 a multi-line
