4e77f340eae15632a7569f6b35b79e129f80166d
Nepcam <cameron@aux.network>
2019-05-04 19:35:35 +1200

    querySelectors

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// This how we select things from the dom.
+
 // Single element selectors
 const ie = document.getElementById('ie=edge');
 const query = document.querySelector('.container');
@@ -5,3 +7,17 @@ const query = document.querySelector('.container');
 // Multiple element selectors
 document.querySelectorAll('.item');
 document.getElementsByClassName('item');
+document.getElementsByTagName('li');
+
+
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => console.log(item));
+
+const ul = document.querySelector('.items'); // taking the ul with the class of items
+
+ul.remove();
+ul.lastElementChild.remove();
+
+ul.firstElementChild.textContent = 'Hello';
+
