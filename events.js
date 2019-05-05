660662b3eaad87dcb4417bcf27246116a7200984
Nepcam <cameron@aux.network>
2019-05-05 20:42:35 +1200

    events

---
+++ events.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('btn');
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e.target.id);
+    document.querySelector('#my-form').style.getElementsByClassName.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Cams the man!!</h1>'
+})
--- selector.js
+++ selector.js
@@ -5,14 +5,14 @@ const ie = document.getElementById('ie=edge');
 const query = document.querySelector('.container');
 
 // Multiple element selectors
-document.querySelectorAll('.item');
-document.getElementsByClassName('item');
-document.getElementsByTagName('li');
+document.querySelectorAll('.item'); // Can more than one
+document.getElementsByClassName('item'); // Can only be classes
+document.getElementsByTagName('li'); // Can only use <tags><tags/>
 
 
 const items = document.querySelectorAll('.item');
 
-items.forEach((item) => console.log(item));
+items.forEach((item) => console.log(item)); // loops through each item and takes in the li
 
 const ul = document.querySelector('.items'); // taking the ul with the class of items
 
@@ -20,4 +20,9 @@ ul.remove();
 ul.lastElementChild.remove();
 
 ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Cam';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
 
+
+const btn = document.querySelector('btn');
+btn.getElementsByClassName.background = 'green'
